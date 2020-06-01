526236a723b867ddaa18ef68fa1112fca361840b
Zach Bassett <basszac@lin2dv2nba56.chrobinson.com>
2020-06-01 15:21:35 -0500

    integrate redis

--- app/app.py
+++ app/app.py
@@ -1,11 +1,37 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
+import redis
+import os
+import json
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version='1.0', title='SmartBid API',
+    description='Back end for the SmartBid application',
+)
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+ns = api.namespace('v1', description='Application operations')
+
+todo = api.model('CustomerData', {
+    'customer_id': fields.String(readonly=True, description='The customer unique identifier'),
+    'data': fields.String(required=True, description='The data payload')
+})
+
+r = redis.Redis(
+    host='redis',
+    port=6379,
+    db=0
+)
+
+@ns.route('/customer/<string:entity_id>')
+class CustomerData(Resource):
+    def get(self, entity_id):
+        redis_payload = r.get(entity_id)
+        decoded = redis_payload.decode()
+
+        return json.loads(decoded)
+
+    @ns.expect(todo)
+    def post(self, entity_id):
+        r.set(entity_id, json.dumps(api.payload))
+
 
