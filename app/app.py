71f24efc6790e744fe50bbfaf6966e406eb0c9a2
Zach Bassett <basszac@lin2dv2nba56.chrobinson.com>
2020-06-02 10:51:33 -0500

    documentation

--- app/app.py
+++ app/app.py
@@ -44,20 +44,25 @@ class CustomerData(Resource):
     def get(self, entity_id):
         '''Retrieve an entity's data from the database'''
         redis_payload = r.get(entity_id)
-        decoded = redis_payload.decode()
+        if redis_payload:
+            decoded = redis_payload.decode()
 
-        return json.loads(decoded)
+            return json.loads(decoded)
+        else:
+            api.abort(404, f"Entity '{ entity_id }' doesn't exist")
 
     @ns.expect(todo)
+    @ns.response(201, 'Entity entered')
     def post(self, entity_id):
-        '''Create a new entity and add data'''
+        '''Create a new entity (or modify existing) and add data'''
         r.set(entity_id, json.dumps(api.payload))
-        return entity_id
+        return entity_id, 201
 
+    @ns.response(204, 'Entity deleted')
     def delete(self, entity_id):
         '''Delete all data for an entity'''
         r.delete(entity_id)
-        return entity_id
+        return entity_id, 204
 
 
 @ns.route('/list_entities')
@@ -69,4 +74,3 @@ class EntityList(Resource):
         for i in keys:
             decoded_list.append(i.decode())
         return decoded_list
-
